eff7523eca29d5ed2dd2496288aae89f11e6035f
fabyolalera <fabyolalera@gmail.com>
2022-10-27 01:41:55 -0300

    Se agrega formulario de contacto

--- academia/views.py
+++ academia/views.py
@@ -1,6 +1,7 @@
 from multiprocessing import context
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib import messages
 
 from academia.forms import ContactoForm   
 
@@ -9,13 +10,21 @@ def index(request):
 
 def contacto(request):
     if request.method == 'POST':
-        formulario = ContactoForm(request.POST)
-        print(request.POST)
+        contact_form = ContactoForm(request.POST)
+        #print(request.POST)
         
-    context = {
-          'formulario' : ContactoForm()    
-        }       
-    return render(request, 'contacto.html', context=context)
+        if(contact_form.is_valid()):
+           # pass
+            messages.success(request,'Gracias por contactarse.')
+            #messages.info(request,'Otro mensajito')
+        else:
+            messages.warning(request,'Corrija los errores')
+    else: 
+        
+        #context = {
+            contact_form = ContactoForm() 
+        #    }       
+    return render(request, 'contacto.html', {'contact_form':contact_form},)
 
 def galeria(request):
     return render(request, 'galeria.html', context={})
