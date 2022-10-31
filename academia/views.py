f31a30691edf89a922524315facf1dd18e54de3c
fabyolalera <fabyolalera@gmail.com>
2022-10-31 00:06:10 -0300

    actulizacion

--- academia/views.py
+++ academia/views.py
@@ -1,13 +1,11 @@
 from multiprocessing import context
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.contrib import messages
-<<<<<<< HEAD
-from ritmos.forms import ContactoForm
-=======
 
-from academia.forms import ContactoForm   
->>>>>>> 944275e4f9f792a1444615beb9ad5fb93677c387
+from django.contrib import messages
+from django.http import HttpResponse
+from django.shortcuts import render
+
+from ritmos.forms import ContactoForm
+
 
 def index(request):
     return render(request, 'index.html', context={})
@@ -15,17 +13,12 @@ def index(request):
 
 def contacto(request):
     if request.method == 'POST':
-<<<<<<< HEAD
+
         formulario = ContactoForm(request.POST)
         #print(request.POST)
         
         if(formulario.is_valid()):
-=======
-        contact_form = ContactoForm(request.POST)
-        #print(request.POST)
-        
-        if(contact_form.is_valid()):
->>>>>>> 944275e4f9f792a1444615beb9ad5fb93677c387
+
            # pass
             messages.success(request,'Gracias por contactarse.')
             #messages.info(request,'Otro mensajito')
@@ -34,16 +27,12 @@ def contacto(request):
     else: 
         
         #context = {
-<<<<<<< HEAD
+
             formulario = ContactoForm() 
         #    }       
     return render(request, 'contacto.html', {'formulario':formulario},)
 
-=======
-            contact_form = ContactoForm() 
-        #    }       
-    return render(request, 'contacto.html', {'contact_form':contact_form},)
->>>>>>> 944275e4f9f792a1444615beb9ad5fb93677c387
+
 
 def galeria(request):
     return render(request, 'galeria.html', context={})
